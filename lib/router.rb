3c17dcd5ae6ff1025718a0652b613820715afb43
Sofia Besenski <sofia@super.gd>
2021-08-16 14:58:03 -0700

    extract parameters from the request url

--- lib/router.rb
+++ lib/router.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Router
   Route = Struct.new(:verb, :path, :action, keyword_init: true)
   Response = Struct.new(:status, :content, keyword_init: true)
@@ -7,14 +8,38 @@ class Router
 
   def call(env)
     # search through list
-    req = Request.from_env(env)
-    if route = @routes.find { |route| req.path == route.path && req.verb == route.verb }
-      Response.new(content: route.action.call, status: 200)
+    @incoming_req = Request.from_env(env)
+    if route = @routes.find { |route| matching?(route) }
+      Response.new(content: route.action.call(@incoming_req), status: 200)
     elsif @fallback_action
-      Response.new(content: @fallback_action.call(req), status: 404)
+      Response.new(content: @fallback_action.call(@incoming_req), status: 404)
     else
       Response.new(content: "Not found", status: 404)
     end
+  end
+  # hello/sitesearch
+  def matching?(route)
+    return false if @incoming_req.verb != route.verb
+
+#TODO get the query params!!!
+    inc_path_chunks = @incoming_req.path.split('/')
+    path_chunks = route.path.split('/')
+    return false if inc_path_chunks.length != path_chunks.length
+
+    inc_path_chunks.zip(path_chunks).map{ |inc_chunk,route_chunk| chunk_match?(inc_chunk, route_chunk) }
+  end
+
+  def chunk_match? (inc_chunk, route_chunk)
+    if route_chunk.start_with?(":")
+      add_param(inc_chunk, route_chunk[1..-1])
+      return true
+    end
+
+    inc_chunk == route_chunk
+  end
+
+  def add_param (param_value, param_key)
+    @incoming_req.params[param_key.to_sym]=param_value
   end
 
   def get(arg, &block)
@@ -37,10 +62,14 @@ class Router
 end
 
 class Request
-  attr_reader :verb, :path
+  attr_reader :verb, :path, :params
   def initialize(verb:, path:)
     @verb = verb
     @path = path
+  end
+
+  def params
+    @params ||= {}
   end
 
   def self.from_env(env)
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -3,12 +3,12 @@ require "pry"
 RSpec.describe Router do
   describe("#call") do
     let(:router) { described_class.new }
-    let(:env) { {"REQUEST_METHOD" => "GET", "REQUEST_PATH" => "/big/long/path"} }
+    let(:env) { {"REQUEST_METHOD" => "GET", "REQUEST_PATH" => "/users/sofia/element/2"} }
     subject { router.call(env) }
 
     it "responds" do
-      router.get("/big/long/path") { "Hello World" }
-      expect(subject.content).to eq("Hello World")
+      router.get("/users/:slug/element/:id") { |req| "Hello #{req.params[:slug]} at element #{req.params[:id]}" }
+      expect(subject.content).to eq("Hello sofia at element 2")
       expect(subject.status).to eq(200)
     end
 
@@ -20,7 +20,7 @@ RSpec.describe Router do
 
       it "responds with a defined 404 fallback when specified" do
         router.not_found { |req| "Special fallback: not found #{req}" }
-        expect(subject.content).to eq("Special fallback: not found /big/long/path")
+        expect(subject.content).to eq("Special fallback: not found /users/sofia/element/2")
         expect(subject.status).to eq(404)
       end
     end
